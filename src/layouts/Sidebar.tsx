be13fca65e9e27e70923496c87ec1be0aeb1a66b
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-08-27 19:33:40 +0600

    sidebar ready

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { HomeOutlined, TranslationOutlined } from '@ant-design/icons';
+import { HomeFilled, TranslationOutlined, BellFilled, QuestionCircleFilled } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
 
-const items2: MenuProps['items'] = [
-    { icon: <HomeOutlined />, label: "Home" },
+const items1: MenuProps['items'] = [
+    { icon: <HomeFilled />, label: "Home" },
     { icon: <TranslationOutlined />, label: "Translated files" },
+  ].map(
+    (item, index) => {
+      return {
+        key: `sub${index + 1}`,
+        icon: item.icon,
+        label: item.label
+      };
+    },
+  );
+const items2: MenuProps['items'] = [
+    { icon: <BellFilled />, label: "Notification" },
+    { icon: <QuestionCircleFilled />, label: "Help ?" },
   ].map(
     (item, index) => {
       return {
@@ -18,14 +30,25 @@ const items2: MenuProps['items'] = [
 
 const Sidebar : React.FC = () => {
     return (
+        <div className='flex flex-col h-full justify-between'>
         <div>
-        <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ borderRight: 0 }}
-            items={items2}
-        />
+          <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ borderRight: 0 }}
+              items={items1}
+          />
+        </div>
+        <div>
+          <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ borderRight: 0 }}
+              items={items2}
+          />
+        </div>
         </div>
     );
 };
